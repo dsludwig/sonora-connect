0fef7148ba59ad2eb2962b23798d38dc25fd2e29
Derek Ludwig <derek.s.ludwig@gmail.com>
2024-11-09 19:24:22 -0800

    test: HTTP_2 protocol

--- conformance/server_runner.py
+++ conformance/server_runner.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
+import logging
+import pathlib
+import shutil
+import socket
 import sys
+import tempfile
 import threading
+import time
 
 import daphne
 import daphne.server
@@ -9,6 +15,11 @@ from connectrpc.conformance.v1.server_compat_pb2 import (
     ServerCompatRequest,
     ServerCompatResponse,
 )
+
+
+def get_open_port() -> int:
+    with socket.create_server(("", 0)) as server:
+        return server.getsockname()[1]
 
 
 def main():
@@ -20,10 +31,34 @@ def main():
         sys.stderr.write("Invalid ServerCompatRequest on stdin")
         sys.exit(1)
 
+    logging.basicConfig(
+        level={
+            0: logging.WARN,
+            1: logging.INFO,
+            2: logging.DEBUG,
+            3: logging.DEBUG,  # Also turns on asyncio debug
+        }[1],
+        format="%(asctime)-15s %(levelname)-8s %(message)s",
+    )
+
     wait = threading.Barrier(1)
+    port = get_open_port()
+    endpoint = f"tcp:port={port}:interface=localhost"
+    response = ServerCompatResponse(host="localhost", port=port)
+
+    tmpdir = None
+    if request.use_tls:
+        tmpdir = pathlib.Path(tempfile.mkdtemp())
+        server_creds = request.server_creds
+        keyfile = tmpdir / "key.pem"
+        certfile = tmpdir / "cert.pem"
+        keyfile.write_bytes(server_creds.key)
+        certfile.write_bytes(server_creds.cert)
+        response.pem_cert = server_creds.cert
+        endpoint = f"ssl:port={port}:interface=localhost:privateKey={str(keyfile)}:certKey={str(certfile)}"
     server = daphne.server.Server(
         asgi_app,
-        endpoints=["tcp:port=8080:interface=localhost"],
+        endpoints=[endpoint],
         ready_callable=wait.reset,
     )
 
@@ -33,13 +68,20 @@ def main():
     threading.Thread(target=run_server).start()
     wait.wait()
 
-    response = ServerCompatResponse(host="localhost", port=8080)
-
     response_buf = response.SerializeToString()
     response_size = len(response_buf)
     sys.stdout.buffer.write(response_size.to_bytes(length=4, byteorder="big"))
     sys.stdout.buffer.write(response_buf)
     sys.stdout.buffer.flush()
+
+    try:
+        while True:
+            time.sleep(3600)
+    except KeyboardInterrupt:
+        # TODO: we're being killed with some other signal...
+        server.stop()
+        if tmpdir is not None:
+            shutil.rmtree(tmpdir)
 
 
 if __name__ == "__main__":
