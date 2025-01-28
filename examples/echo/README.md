97784a3aead61d99b076e562c0551ac24d2850fb
Derek Ludwig <derek.s.ludwig@gmail.com>
2025-01-27 18:23:40 -0800

    add asgi/asyncio echo example

--- examples/echo/README.md
+++ examples/echo/README.md
@@ -4,3 +4,29 @@ A nice simple example showing various method types and streaming modes.
 
 Copied almost entirely from the
 [official grpc-web repo](https://github.com/grpc/grpc-web/blob/92aa9f8fc8e7af4aadede52ea075dd5790a63b62/net/grpc/gateway/examples/echo/echo.proto).
+
+## How to run
+
+Start a server:
+
+```
+python wsgi.py
+```
+
+OR
+
+```
+python asgi.py
+```
+
+Then run one of the clients:
+
+```
+python client.py
+```
+
+OR
+
+```
+python aclient.py
+```
---
+++ examples/echo/aclient.py
@@ -0,0 +1,33 @@
+import asyncio
+
+from google.protobuf.duration_pb2 import Duration
+import sonora.aio
+import echo_pb2_grpc, echo_pb2
+
+
+async def example():
+    async with sonora.aio.insecure_web_channel("http://localhost:8888") as c:
+        x = echo_pb2_grpc.EchoServiceStub(c)
+        d = Duration(seconds=1)
+
+        r = await x.Echo(echo_pb2.EchoRequest(message="beep"))
+        print(r)
+
+        with x.ServerStreamingEcho(
+            echo_pb2.ServerStreamingEchoRequest(
+                message="honk", message_count=10, message_interval=d
+            )
+        ) as call:
+            async for r in call:
+                print(r)
+
+        with x.ServerStreamingEchoAbort(
+            echo_pb2.ServerStreamingEchoRequest(
+                message="honk", message_count=10, message_interval=d
+            )
+        ) as call:
+            async for r in call:
+                print(r)
+
+
+asyncio.run(example())
